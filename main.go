3102bcbabc2e4fe275f32805572ca8ca592a1582
Robin Miller <robin.miller@atruvia.de>
2025-11-11 07:08:38 +0100

    chore: added base htmx hello stub

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("internal/adapters/htmlx/hello.html"))
+	tmpl.Execute(w, map[string]string{"Name": "HTMX + Go"})
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
