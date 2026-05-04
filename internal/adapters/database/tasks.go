4132be396698c6fbcffa083ce02151236f760ade
Rob <robwork91@gmail.com>
2026-05-04 18:28:35 +0200

    chore: setup create and load snapshot

--- internal/adapters/database/tasks.go
+++ internal/adapters/database/tasks.go
@@ -32,3 +32,31 @@ func (s *SQLiteAdapter) LoadSnapshot(ctx context.Context, projectID string) ([]p
 
 	return tasks, nil
 }
+
+func (s *SQLiteAdapter) CreateSnapshot(ctx context.Context, projectID string, tasks []ports.Task) error {
+	tx, err := s.Conn.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec(`DELETE FROM tasks WHERE project_id = ?`, projectID)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(`INSERT INTO tasks (id, name, description, project_id) VALUES (?, ?, ?, ?)`)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, t := range tasks {
+		_, err = stmt.Exec(t.ID, t.Name, t.Description, projectID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
